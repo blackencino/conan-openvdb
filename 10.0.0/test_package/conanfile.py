57c8af431ffa91d8cfd2062d01b4b461ab331359
Eric Shi <ershi@nvidia.com>
2023-01-17 08:41:37 -0800

    Update test_package

--- 10.0.0/test_package/conanfile.py
+++ 10.0.0/test_package/conanfile.py
@@ -1,20 +1,29 @@
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import cmake_layout, CMake
 import os
 
-from conans import ConanFile, CMake, tools
 
-class OpenVDBTestConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake_find_package"
+class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    test_type = "explicit"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def build(self):
-        cmake = CMake(self, generator='Ninja')
-        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is
-        # in "test_package"
+        cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
-            bin_path = os.path.join(".", "example")
-            usdfile = os.path.join(self.source_folder, "..", "..", "example_data", "example.vdb")
-            self.run("{} {}".format(bin_path, usdfile), run_environment=True)
+        if can_run(self):
+            bin_path = os.path.join(self.cpp.build.bindirs[0], "example")
+            usdfile = os.path.join(
+                self.source_folder, "..", "..", "example_data", "example.vdb"
+            )
+            self.run("{} {}".format(bin_path, usdfile), env="conanrun")
